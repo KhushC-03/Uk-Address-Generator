10feb38d54a181b893d2c823ac08543374ddc549
Khush Chauhan <68359616+KhushC-03@users.noreply.github.com>
2020-11-17 22:21:17 +0000

    Update #Uk_Address_Generator.py

--- #Uk_Address_Generator.py
+++ #Uk_Address_Generator.py
@@ -1,35 +1,57 @@
-# Uk Address Generator
 import requests, os
 def addygen():
     os.system('cls')
-    amount = input('\x1b[1;37mHow Many Addresses Would You Link To Generate\x1b[1;36m:\x1b[1;37m ')
-    print("""            AL B BA BB BD BH BL BN BR BS CA CB CF CH CM CO CR CT CV CW D
-            A DE DH DL DN DT DY E EC EN EX FY GL GU HA HD HG HP HR HU HX
-            IG IP KT L LA LD LE LL LN LS LU M ME MK N NE NG NN NP NR NW OL 
-            OX PE PL PO PR RG RH RM S SA SE SG SK SL SM SN SO SP SR SS ST 
-            SW SY TA TF TN TQ TR TS TW UB W WA WC WD WF WN WR WS WV YO""")
-    postcode = input('\x1b[1;37mPost Code Prefix \x1b[1;36m:\x1b[1;37m ')
-    headers = {
-        'authority': 'www.doogal.co.uk',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36 Edg/85.0.564.51',
-        'referer': 'https://www.doogal.co.uk/RandomAddresses.php'
-    }
-    params = {
-        'startWith': postcode
-    }
-    for i in range(int(amount)):
-        r = requests.get('https://www.doogal.co.uk/CreateRandomAddress.ashx', headers=headers, params=params)
-        adddress = r.text
-        addy = adddress.split(',')
-        a = addy[0].split(',')[-1]
-        b = addy[1].split(',')[-1]
-        c = addy[2].split(',')[-1]
-        d = addy[3].split(',')[-1]
-        mainaddress = ('{} {} {} {} \n').format(a,b,c,d)
-        mainaddress1 = ('{} {} {} {}').format(a,b,c,d)
+    address_file = os.path.exists('addesses.txt')
+    if address_file == False:
+        a = open('addesses.txt','w')
+        addygen()
+    elif address_file == True:
+        amount = int(input('\x1b[1;37mHow Many Addresses Would You Link To Generate\x1b[1;36m?\x1b[1;37m '))
+        print("""                AL B BA BB BD BH BL BN BR BS CA CB CF CH CM CO CR CT CV CW D
+                A DE DH DL DN DT DY E EC EN EX FY GL GU HA HD HG HP HR HU HX
+                IG IP KT L LA LD LE LL LN LS LU M ME MK N NE NG NN NP NR NW OL 
+                OX PE PL PO PR RG RH RM S SA SE SG SK SL SM SN SO SP SR SS ST 
+                SW SY TA TF TN TQ TR TS TW UB W WA WC WD WF WN WR WS WV YO""")
+        postcode = input('\x1b[1;37mPost Code Prefix \x1b[1;36m:\x1b[1;37m ')
+        headers = {
+            'authority': 'www.doogal.co.uk',
+            'accept': '*/*',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36 Edg/85.0.564.51',
+            'x-requested-with': 'XMLHttpRequest',
+            'sec-fetch-site': 'same-origin',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-dest': 'empty',
+            'referer': 'https://www.doogal.co.uk/RandomAddresses.php',
+            'accept-language': 'en-GB,en;q=0.9,en-US;q=0.8',
+            'cookie': '_ga=GA1.3.926399585.1600422616; _gid=GA1.3.619049017.1600422616; _gat=1',
+        }
         addys = open('addesses.txt','a')
-        addys.write(str(mainaddress))
-        print(mainaddress1)
-    addys.close()
-os.system('cls')
+        params = ('startWith', postcode),
+        
+        for i in range(amount):
+            r = requests.get('https://www.doogal.co.uk/CreateRandomAddress.ashx', headers=headers, params=params)
+            address = r.text
+            if len(address.split(','))==3:
+                addy = address.split(',')
+                mainaddress = ('{} {} {} \n').format(addy[0],addy[1],addy[2])
+                mainaddress1 = ('\x1b[1;36m{} {} {}').format(addy[0],addy[1],addy[2])
+                addys.write(str(mainaddress))
+                print(mainaddress1)
+            elif len(address.split(','))==4:
+                addy = address.split(',')
+                mainaddress = ('{} {} {} \n').format(addy[0],addy[1],addy[2])
+                mainaddress1 = ('\x1b[1;36m{} {} {}').format(addy[0],addy[1],addy[2])
+                addys.write(str(mainaddress))
+                print(mainaddress1)
+            elif len(address.split(','))==5:
+                addy = address.split(',')
+                mainaddress = ('{} {} {} \n').format(addy[0],addy[1],addy[2],addy[3])
+                mainaddress1 = ('\x1b[1;36m{} {} {}').format(addy[0],addy[1],addy[2],addy[3])
+                addys.write(str(mainaddress))
+                print(mainaddress1)
+        addys.close()  
+        print('Finished')
+
+    else:
+        print('\x1b[1;37mFile Error')   
 addygen()
